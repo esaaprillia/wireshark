fdba90d520e6bd6a4bf4f4d6b83eb8eb8e855d1a
esaaprillia <esaapriliasalsabila@gmail.com>
2024-09-10 10:47:29 +0800

    Update FindNL.cmake

--- cmake/modules/FindNL.cmake
+++ cmake/modules/FindNL.cmake
@@ -1,129 +1,97 @@
 #
+# $Id$
+#
 # Find the native netlink includes and library
 #
 # If they exist, differentiate between versions 1, 2 and 3.
 # Version 1 does not have netlink/version.h
-# Version 2 started separating libraries (libnl{,-genl,-route}).
-# Version 3 (>= 3.2) started appending the major version number as suffix to
-# library names (libnl-3)
+# Version 3 does have the major version number as a suffix
+#   to the libnl name (libnl-3)
 #
 #  NL_INCLUDE_DIRS - where to find libnl.h, etc.
-#  NL_LIBRARIES    - List of libraries when using libnl.
+#  NL_LIBRARIES    - List of libraries when using libnl3.
 #  NL_FOUND        - True if libnl found.
 
-if(NL_LIBRARIES AND NL_INCLUDE_DIRS)
+IF (NL_LIBRARIES AND NL_INCLUDE_DIRS )
   # in cache already
   SET(NL_FOUND TRUE)
-else()
+ELSE (NL_LIBRARIES AND NL_INCLUDE_DIRS )
   SET( SEARCHPATHS
       /opt/local
       /sw
       /usr
       /usr/local
   )
-
-  find_package(PkgConfig)
-  pkg_check_modules(NL3 libnl-3.0 libnl-genl-3.0 libnl-route-3.0)
-  if(NOT NL3_FOUND)
-    pkg_search_module(NL2 libnl-2.0)
-  endif()
-
-  # Try to find NL 2.0, 3.0 or 3.1 (/usr/include/netlink/version.h) or
-  # NL >= 3.2 (/usr/include/libnl3/netlink/version.h)
-  find_path(NL3_INCLUDE_DIR
+  FIND_PATH( NL_INCLUDE_DIR
     PATH_SUFFIXES
       include/libnl3
-      include
     NAMES
       netlink/version.h
-    HINTS
-      "${NL3_libnl-3.0_INCLUDEDIR}"
-      "${NL2_INCLUDEDIR}"
     PATHS
       $(SEARCHPATHS)
   )
   # NL version >= 2
-  if(NL3_INCLUDE_DIR)
-    find_library(NL3_LIBRARY
+  IF ( NL_INCLUDE_DIR )
+    FIND_LIBRARY( NL_LIBRARY
       NAMES
         nl-3 nl
       PATH_SUFFIXES
         lib64 lib
-      HINTS
-        "${NL3_libnl-3.0_LIBDIR}"
-        "${NL2_LIBDIR}"
       PATHS
         $(SEARCHPATHS)
     )
-    find_library(NLGENL_LIBRARY
+    FIND_LIBRARY( NLGENL_LIBRARY
       NAMES
         nl-genl-3 nl-genl
       PATH_SUFFIXES
         lib64 lib
-      HINTS
-        "${NL3_libnl-genl-3.0_LIBDIR}"
-        "${NL2_LIBDIR}"
       PATHS
         $(SEARCHPATHS)
     )
-    find_library(NLROUTE_LIBRARY
+    FIND_LIBRARY( NLROUTE_LIBRARY
       NAMES
         nl-route-3 nl-route
       PATH_SUFFIXES
         lib64 lib
-      HINTS
-        "${NL3_libnl-route-3.0_LIBDIR}"
-        "${NL2_LIBDIR}"
       PATHS
         $(SEARCHPATHS)
     )
     #
     # If we don't have all of those libraries, we can't use libnl.
     #
-    if(NL3_LIBRARY AND NLGENL_LIBRARY AND NLROUTE_LIBRARY)
-      set(NL_LIBRARY ${NL3_LIBRARY})
-      if(NL3_INCLUDE_DIR)
-        # NL2 and NL3 are similar and just affect how the version is reported in
-        # the --version output. In cast of doubt, assume NL3 since a library
-        # without version number could be any of 2.0, 3.0 or 3.1.
-        if(NOT NL3_FOUND AND NL2_FOUND)
-          set(HAVE_LIBNL2 1)
-        else()
-          set(HAVE_LIBNL3 1)
-        endif()
-      endif()
-    endif()
-    set(NL_INCLUDE_DIR ${NL3_INCLUDE_DIR})
-  endif()
-
-  # libnl-2 and libnl-3 not found, try NL version 1
-  if(NOT (NL_LIBRARY AND NL_INCLUDE_DIR))
-    pkg_search_module(NL1 libnl-1)
-    find_path(NL1_INCLUDE_DIR
+    IF ( NOT NLGENL_LIBRARY AND NOT NLROUTE_LIBRARY )
+      SET( NL_LIBRARY NOTFOUND )
+    ENDIF ( NOT NLGENL_LIBRARY AND NOT NLROUTE_LIBRARY )
+    IF( NL_LIBRARY )
+      STRING(REGEX REPLACE ".*nl-([^.,;]*).*" "\\1" NLSUFFIX ${NL_LIBRARY})
+      IF ( NLSUFFIX )
+        SET( HAVE_LIBNL3 1 )
+      ELSE ( NLSUFFIX )
+        SET( HAVE_LIBNL2 1 )
+      ENDIF (NLSUFFIX )
+      SET( HAVE_LIBNL 1 )
+    ENDIF( NL_LIBRARY )
+  ELSE( NL_INCLUDE_DIR )
+    # NL version 1 ?
+    FIND_PATH( NL_INCLUDE_DIR
       NAMES
         netlink/netlink.h
-      HINTS
-        "${NL1_INCLUDEDIR}"
       PATHS
         $(SEARCHPATHS)
     )
-    find_library(NL1_LIBRARY
+    FIND_LIBRARY( NL_LIBRARY
       NAMES
         nl
       PATH_SUFFIXES
         lib64 lib
-      HINTS
-        "${NL1_LIBDIR}"
       PATHS
         $(SEARCHPATHS)
     )
-    set(NL_LIBRARY ${NL1_LIBRARY})
-    set(NL_INCLUDE_DIR ${NL1_INCLUDE_DIR})
-    if(NL1_LIBRARY AND NL1_INCLUDE_DIR)
-      set(HAVE_LIBNL1 1)
-    endif()
-  endif()
-endif()
+    if ( NL_INCLUDE_DIR )
+      SET( HAVE_LIBNL1 1 )
+    ENDIF ( NL_INCLUDE_DIR )
+  ENDIF( NL_INCLUDE_DIR )
+ENDIF(NL_LIBRARIES AND NL_INCLUDE_DIRS)
 # MESSAGE(STATUS "LIB Found: ${NL_LIBRARY}, Suffix: ${NLSUFFIX}\n  1:${HAVE_LIBNL1}, 2:${HAVE_LIBNL2}, 3:${HAVE_LIBNL3}.")
 
 # handle the QUIETLY and REQUIRED arguments and set NL_FOUND to TRUE if
@@ -132,13 +100,12 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(NL DEFAULT_MSG NL_LIBRARY NL_INCLUDE_DIR)
 
 IF(NL_FOUND)
-  set(NL_LIBRARIES ${NLGENL_LIBRARY} ${NLROUTE_LIBRARY} ${NL_LIBRARY})
-  set(NL_INCLUDE_DIRS ${NL_INCLUDE_DIR})
-  set(HAVE_LIBNL 1)
-else()
-  set(NL_LIBRARIES )
-  set(NL_INCLUDE_DIRS)
-endif()
+  SET( NL_LIBRARIES ${NLGENL_LIBRARY} ${NLROUTE_LIBRARY} ${NL_LIBRARY} )
+  SET( NL_INCLUDE_DIRS ${NL_INCLUDE_DIR})
+ELSE()
+  SET( NL_LIBRARIES )
+  SET( NL_INCLUDE_DIRS )
+ENDIF()
 
 MARK_AS_ADVANCED( NL_LIBRARIES NL_INCLUDE_DIRS )
 
